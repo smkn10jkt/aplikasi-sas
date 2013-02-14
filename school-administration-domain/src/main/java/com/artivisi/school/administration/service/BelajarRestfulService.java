1074edc6c7c5fdc941673d78e1e9bc6973238443
kinanti.pngkan@ymail.com <kinanti.pingkan@ymail.com>
2013-02-14 15:53:35 +0700

    service student, lessons, & job training

--- school-administration-domain/src/main/java/com/artivisi/school/administration/service/BelajarRestfulService.java
+++ school-administration-domain/src/main/java/com/artivisi/school/administration/service/BelajarRestfulService.java
@@ -3,9 +3,12 @@ package com.artivisi.school.administration.service;
 import java.util.List;
 
 import com.artivisi.school.administration.domain.ApplicationConfig;
+import com.artivisi.school.administration.domain.Job;
+import com.artivisi.school.administration.domain.Lesson;
 import com.artivisi.school.administration.domain.Menu;
 import com.artivisi.school.administration.domain.Permission;
 import com.artivisi.school.administration.domain.Role;
+import com.artivisi.school.administration.domain.Student;
 import com.artivisi.school.administration.domain.Teacher;
 import com.artivisi.school.administration.domain.User;
 import java.util.List;
@@ -61,4 +64,25 @@ public interface BelajarRestfulService extends MonitoredService {
     Teacher findTeacherById(String id);
     Page<Teacher> findAllTeacher();
     Long countAllTeachers();
+    
+    // lessons
+    void save(Lesson lesson);
+    void delete(Lesson lesson);
+    Lesson findLessonById(String id);
+    Page<Lesson> findAllLessons();
+    Long countAllLesson();
+    
+    // student
+    void save(Student student);
+    void delete(Student student);
+    Lesson findStudentById(String id);
+    Page<Lesson> findAllStudent();
+    Long countAllStudent();
+    
+    // job training
+    void save(Job job);
+    void delete(Job job);
+    Lesson findJobById(String id);
+    Page<Lesson> findAllJob();
+    Long countAllJob();
 }
