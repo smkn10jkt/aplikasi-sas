85dc74deb358a34eed093c100f85752cccba90af
Muhammad Fariz <dsemuts@gmail.com>
2013-02-14 10:27:03 +0700

    domain teacher

---
+++ school-administration-domain/src/main/java/com/artivisi/school/administration/domain/Teacher.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.school.administration.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author dsemuts
+ */
+@Entity @Table(name="data_kelas")
+public class Teacher {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+    
+    @NotNull @NotEmpty
+    @Column(nullable = false)
+    private String nama;
+    
+    @NotNull @NotEmpty
+    @Column(nullable = false)
+    private String nip;
+    
+    @NotNull @NotEmpty
+    @Column(nullable = true)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+    
+    
+}
