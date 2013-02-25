9f5518c6bfd8b98ef183296666ff970ab9e241e1
jcjack <jhendrikwillem@yahoo.com>
2013-02-24 22:05:31 -0800

    Merelasikan Master Majors ke Master Class

--- school-administration-domain/src/main/java/com/artivisi/school/administration/domain/Kelas.java
+++ school-administration-domain/src/main/java/com/artivisi/school/administration/domain/Kelas.java
@@ -8,6 +8,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.GenericGenerator;
@@ -26,9 +28,10 @@ public class Kelas {
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     private String id;
     
-    @NotNull @NotEmpty
-    @Column(nullable = false)
-    private String majors;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "majors", nullable = false)
+    private Major majors;
     
     @NotNull @NotEmpty
     @Column(nullable = false)
@@ -66,14 +69,15 @@ public class Kelas {
         this.id = id;
     }
 
-    public String getMajors() {
+    public Major getMajors() {
         return majors;
     }
 
-    public void setMajors(String majors) {
+    public void setMajors(Major majors) {
         this.majors = majors;
     }
 
+   
     public String getName() {
         return name;
     }
